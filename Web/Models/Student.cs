3aaccf83804c01836f6c07b69d71d1ee9212ba1a
jasonLiu001 <wangliu@isoftstone.com>
2015-06-08 18:07:04 +0800

    added new
    added new

---
+++ Web/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string name { get; set; }
+    }
+}
