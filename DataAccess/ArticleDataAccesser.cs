0b7fc65d39add8200a3e76c7d73fc8898ebdc9b4
jasonLiu001 <wangliu@isoftstone.com>
2015-06-15 17:14:31 +0800

    added new files
    added new files.

---
+++ DataAccess/ArticleDataAccesser.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess.Helper;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ArticleDataAccesser
+    {
+        public List<Article> GetArticleList()
+        {
+            List<Article> list=new List<Article>();
+            string cmdText = "select top 10 title,site_name,url,media_type,polarity,publish_date,same_doc_count,address,content from original_news";
+            DataSet ds = SqlHelper.ExecuteDataset(SqlHelper.ConnStr, CommandType.Text, cmdText);
+            DataTable dt = ds.Tables[0];
+            foreach (DataRow dr in dt.Rows)
+            {
+                var article = new Article(){
+                    title=Utility.GetColumnValue(dr,"title"),
+                    site_name = Utility.GetColumnValue(dr, "site_name"),
+                    url = Utility.GetColumnValue(dr, "url"),
+                    media_type = Utility.GetColumnValue(dr, "media_type"),
+                    polarity = Utility.GetColumnIntValue(dr, "polarity"),
+                    publish_date = Utility.GetColumnDateTimeValue(dr, "publish_date"),
+                    same_doc_count = Utility.GetColumnIntValue(dr, "same_doc_count"),
+                    address = Utility.GetColumnValue(dr, "address"),
+                    content = Utility.GetColumnValue(dr, "content")
+                };
+                list.Add(article);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ DataAccess/Helper/Utility.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Helper
+{
+    public class Utility
+    {
+        #region parse value
+
+        public static string GetColumnValue(DataRow row, string column)
+        {
+            var result = string.Empty;
+            var curVal = row[column];
+            if (curVal != null && curVal != DBNull.Value)
+            {
+                result = curVal.ToString();
+            }
+            return result;
+        }
+
+        public static int GetColumnIntValue(DataRow row, string column)
+        {
+            var result = 0;
+
+            var tempVal = GetColumnValue(row, column);
+
+            int.TryParse(tempVal, out result);
+
+            return result;
+        }
+
+        public static decimal GetColumnDoubleValue(DataRow row, string column)
+        {
+            decimal result = 0;
+
+            var tempVal = GetColumnValue(row, column);
+
+            decimal.TryParse(tempVal, out result);
+
+            return result;
+        }
+
+        public static decimal GetColumnDecimalValue(DataRow row, string column)
+        {
+            decimal result = 0;
+
+            var tempVal = GetColumnValue(row, column);
+
+            decimal.TryParse(tempVal, out result);
+
+            return result;
+        }
+
+        public static DateTime GetColumnDateTimeValue(DataRow row, string column)
+        {
+            DateTime result = DateTime.MinValue;
+
+            var tempVal = GetColumnValue(row, column);
+
+            if (DateTime.TryParse(tempVal, out result))
+            {
+                //TODO: result = SPUtility.ConvertUtcToPst(result);
+                result = ConvertUtcToPst(result);
+            }
+
+            return result;
+        }
+
+        [System.Obsolete("This is a temporary, Practical method in the SPUtility.cs file.", false)]
+        public static DateTime ConvertUtcToPst(DateTime utc)
+        {
+            return utc.ToLocalTime();
+        }
+
+        #endregion
+    }
+}
