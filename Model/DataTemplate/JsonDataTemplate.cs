133dc41b97215bd735f9b29d4bc52b70a21eb469
jasonLiu001 <wangliu@isoftstone.com>
2015-07-02 16:58:25 +0800

    added new dataTemplate.

--- Model/DataTemplate/JsonDataTemplate.cs
+++ Model/DataTemplate/JsonDataTemplate.cs
@@ -13,6 +13,18 @@ namespace Model.DataTemplate
         public List<T> children { get; set; }
     }
 
+    public class CompassDataTemplate<T1, T2, T3>
+        where T1 : new()
+        where T2 : new()
+        where T3 : new()
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public List<T1> children1 { get; set; }
+        public List<T2> children2 { get; set; }
+        public List<T3> children3 { get; set; }
+    }
+
     public class CommonDataEntity
     {
         public string name { get; set; }
